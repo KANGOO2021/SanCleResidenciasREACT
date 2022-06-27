76d435f5955e34e2af44be823ef2512818558782
Sergio <ser2004munoz@yahoo.com.ar>
2022-06-26 23:04:41 -0300

    sancle

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import mundomarino from "../assets/img/mundo marino.jpg";
-import termasmarinas from "../assets/img/termas1.jpg";
+/* import mundomarino from "../assets/img/mundo marino.jpg";
+import termasmarinas from "../assets/img/termas1.jpg"; */
 import facebook from "../assets/img/facebook-logo-3-1.png";
 
 
 const Home = () => {
-    /* const fecha = () => {
+    const fecha = () => {
         const current = new Date();
         const date = `${current.getFullYear()} `;
         const date2 = `${current.getMonth() + 1}`;
@@ -20,16 +20,16 @@ const Home = () => {
         const current = new Date();
         const date = `${current.getFullYear()}`;
         return (date - origen)
-    } */
+    }
    
     return (  
 <>
         <section className="section" id="home">
             <div className="tex">
-                <p className="p1" id="home2">¡VENI A DISFRUTAR TUS VACACIONES {/* {fecha()} */} EN SAN CLEMENTE DEL TUYÚ!</p>
+                <p className="p1" id="home2">¡VENI A DISFRUTAR TUS VACACIONES {fecha()} EN SAN CLEMENTE DEL TUYÚ!</p>
             </div>
             <div className="tex2">
-                <p className="p2">Visítanos en Facebook, {/* {años()} */} años de alquiler y traslados consecutivos</p>
+                <p className="p2">Visítanos en Facebook, {años()} años de alquiler y traslados consecutivos</p>
                         <a href="https://www.facebook.com/profile.php?id=100007425427174" target="_blank "><img id="face" style={{ width: "50px" }} src={facebook} alt=""/></a>
             </div>
         </section>
@@ -37,7 +37,7 @@ const Home = () => {
         <h2 className="tittle2 ">ATRACTIVOS TURÍSTICOS</h2>
         <hr style={{ width: "90%" }}/>
 
-        <section className="sitios">
+     {/*    <section className="sitios">
 
             <div className="mm">
                         <img style={{ width: "230px" }} src={mundomarino} alt="" />
@@ -48,7 +48,7 @@ const Home = () => {
                         <img style={{width: "190px"}} src={termasmarinas} alt=""/>
                 <button type="button " className="btn "><a href="https://termasmarinas.com.ar/" target="_blank ">SITIO WEB</a></button>
             </div>
-        </section> 
+        </section>  */}
 </>
             
   );
